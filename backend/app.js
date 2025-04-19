af43a5a335843731e29712bc4de71bd3ff6b9818
Alok Jadhao <jadhaoalok51@gmail.com>
2025-04-19 22:59:31 +0530

    Cleaned for merge

--- backend/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-const app = express();
-const PORT = process.env.PORT || 5000;
